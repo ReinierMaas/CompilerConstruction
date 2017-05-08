55d33082c34de55b422dcff338eb6863a50a95b6
Reinier Maas <reiniermaas@hotmail.com>
2017-05-08 20:11:26 +0200

    type incorrect programs from project document

--- tdiagrams/test/Spec.hs
+++ tdiagrams/test/Spec.hs
@@ -64,7 +64,14 @@ correctPrograms = [
 
 typeIncorrectPrograms :: [String]
 typeIncorrectPrograms = [
-    "execute platform x86 on platform ARM end"
+    "execute platform x86 on platform ARM end", -- 1
+    "execute platform x86 on platform x86 end", -- 1
+    "execute program hello in Haskell on program world in Haskell end", -- 2
+    "execute interpreter hugs for Haskell in x86-windows on compiler hugs from Haskell to x86-windows in x86-windows end", -- 2
+    "execute compile program hello in UUAG with compiler uuagc from UUAG to Haskell in i686-windows end on interpreter x64-on-arm for x64 in arm end", -- 3
+    "compile platform x64 with compiler x64-to-x86 from x64 to x86 in x64 end", -- 4
+    "compile program hello in x64 with platform x64 end", -- 5
+    "compile program hello in Haskell with compiler uuagc from UUAG to Haskell in i686-windows end" -- 6
     ]
 
 wrongPrograms :: [String]
