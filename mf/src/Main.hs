d48ddf240d0f36023a8f404df6a82bb96f26e1b0
Adolfo Ochagavía <aochagavia92@gmail.com>
2017-05-15 16:23:25 +0200

    GraphViz

--- mf/src/Main.hs
+++ mf/src/Main.hs
@@ -1,7 +1,32 @@
 module Main where
 
+import Data.GraphViz
+import Data.GraphViz.Attributes (toLabel)
+import Data.GraphViz.Printing (toDot, renderDot)
+import Data.Graph.Inductive.Graph
+import Data.Graph.Inductive.PatriciaTree
+import Data.Text.Lazy (unpack)
+
+import CFG
 import Lexer
 import Parser
 
 main :: IO ()
 main = undefined
+
+myGraph :: Gr String String
+myGraph = foldl (\acc x -> x acc) empty cmds
+    where cmds = [ insNode (1, "x = 1")
+                 , insNode (2, "x == 1")
+                 , insNode (3, "x = x + 2")
+                 , insNode (4, "x = x - 2")
+                 , insEdge (1, 2, "")
+                 , insEdge (2, 3, "true")
+                 , insEdge (2, 4, "false")]
+
+printGraph = let dot = graphToDot params myGraph
+                 code = renderDot $ toDot dot
+             in putStrLn $ unpack code
+    where params = nonClusteredParams { fmtNode = fn, fmtEdge = fe }
+          fn (_, l) = [toLabel l]
+          fe (_, _, l) = [toLabel l]
