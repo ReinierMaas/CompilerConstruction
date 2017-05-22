6c155fbd93cd1c2aef6b206e47d985f1cb8e9273
Adolfo Ochagavía <aochagavia92@gmail.com>
2017-05-22 14:50:29 +0200

    ARGH 2

--- mf/src/Monotone.hs
+++ mf/src/Monotone.hs
@@ -33,13 +33,9 @@ data ConstData = Top
                | Bool Bool
                deriving (Eq, Show)
 
-bool :: ConstData -> Bool
-bool (Bool x) = x
-bool x = error $ "Called bool with wrong argument: " ++ show x
-
 constTransfer :: Stat' -> Maybe (Map String ConstData) -> Maybe (Map String ConstData)
 constTransfer (IAssign' _ name val) (Just input) = Just $ Map.insert name (evalI input val) input
-constTransfer (BAssign' l name val) (Just input) = Just $ Map.insert name (boolToConstData (evalB input val)) input
+constTransfer (BAssign' l name val) (Just input) = Just $ Map.insert name (evalB input val) input
 constTransfer _ input = input
 
 evalI :: Map String ConstData -> IExpr -> ConstData
@@ -51,17 +47,24 @@ evalI input = eval
     eval :: IExpr -> ConstData
     eval (IConst v) = Nat v
     eval (Var n) = fromMaybe Top (Map.lookup n input)
-    eval (Plus x y) = combine (+) $ eval x <*> eval y
-    eval (Minus x y) = combine (-) $ eval x <*> eval y
-    eval (Times x y) = combine (*) $ eval x <*> eval y
-    eval (Divide x y) = combine div $ eval x <*> eval y
+    eval (Plus x y) = combine (+) (eval x) (eval y)
+    eval (Minus x y) = combine (-) (eval x) (eval y)
+    eval (Times x y) = combine (*) (eval x) (eval y)
+    eval (Divide x y) = combine div (eval x) (eval y)
     eval (Deref x) = eval x
 
-evalB :: Map String ConstData -> BExpr -> Maybe Bool
+evalB :: Map String ConstData -> BExpr -> ConstData
 evalB input = eval
     where
+    combineB :: (Bool -> Bool -> Bool) -> ConstData -> ConstData -> ConstData
+    combineB op (Bool x) (Bool y) = Bool (op x y)
+    combineB _ _ _ = Top
+    combineI :: (Int -> Int -> Bool) -> ConstData -> ConstData -> ConstData
+    combineI op (Nat x) (Nat y) = Bool (op x y)
+    combineI _ _ _ = Top
+    eval :: BExpr -> ConstData
     eval (BConst v) = Just v
-    eval (BVar n) = bool <$> Map.lookup n input
+    eval (BVar n) =  Map.lookup n input
     eval _ = undefined
 
 constJoin :: Map String ConstData -> Map String ConstData -> Map String ConstData
