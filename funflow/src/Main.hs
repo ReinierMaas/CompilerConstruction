db75be46edc9a035a5d4f9b46f9b531bf90865bb
Reinier Maas <reiniermaas@hotmail.com>
2017-06-12 17:11:43 +0200

    docopt and executable

--- funflow/src/Main.hs
+++ funflow/src/Main.hs
@@ -1,5 +1,6 @@
 -- (C) 2013-14 Pepijn Kokke & Wout Elsinghorst
 -- Modifications made Jurriaan Hage
+{-# LANGUAGE QuasiQuotes #-}
 
 module Main where
 
@@ -10,32 +11,28 @@ import Control.Monad.State.Lazy (runState)
 import Data.Set (Set)
 import Data.Map (Map)
 
---import System.Console.Docopt (Arguments, Docopt, Option, docopt, parseArgs, exitWithUsage, exitWithUsageMessage, getArgOrExitWith, argument, isPresent, command)
---
---usage :: Docopt
---usage = [docopt|
---funflow version 0.1.0
---Usage:
---  funflow <file>
---  funflow --help | -h
--- |]
---
---getArgOrExit :: Arguments -> Option -> IO String
---getArgOrExit = getArgOrExitWith usage
---
---parseArgsOrExit :: [String] -> IO Arguments
---parseArgsOrExit = either (\err -> exitWithUsageMessage usage (show err)) (\args -> return args) . parseArgs usage
---
---main :: IO ()
---main = do
---    arguments <- getArgs
---    args <- parseArgsOrExit arguments
---    if (args `isPresent` (command "--help")) || (args `isPresent` (command "-h"))
---    then exitWithUsage usage
---    else do
---        file <- args `getArgOrExit` (argument "file")
---        run file
-main = undefined
+import System.Environment (getArgs)
+import System.Console.Docopt (Arguments, Docopt, Option, docopt, parseArgs, exitWithUsage, exitWithUsageMessage, getArgOrExitWith, argument, isPresent, command)
+
+usage :: Docopt
+usage = [docopt|
+funflow version 0.1.0
+Usage:
+  funflow <file>
+|]
+
+getArgOrExit :: Arguments -> Option -> IO String
+getArgOrExit = getArgOrExitWith usage
+
+parseArgsOrExit :: [String] -> IO Arguments
+parseArgsOrExit = either (\err -> exitWithUsageMessage usage (show err)) (\args -> return args) . parseArgs usage
+
+main :: IO ()
+main = do
+    arguments <- getArgs
+    args <- parseArgsOrExit arguments
+    file <- args `getArgOrExit` (argument "file")
+    run file
 
 run :: String -> IO ()
 run name = do
